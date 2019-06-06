ee982f619755a5742d03831e6085da59b18e71d9
csgiraldo <cgiraldovillegas553@gmail.com>
2019-06-06 00:04:02 -0500

    ejercicios con variables

---
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,19 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+
+	public static void main(String args[]) {
+	
+		String nombre="Santiago";
+		double numero=10.05;
+		final double dolar=3200;
+		float pi=3.1234993f;
+		double euler=1.12345789098765;
+	
+		System.out.println("El nombre es: "+nombre);
+		System.out.println("El número es: "+numero);
+		System.out.println("El valor del dolar es: "+dolar);
+		System.out.println("Los 7 decimales del PI son: "+pi);
+		System.out.println("Los decimales del euler son: "+euler);
+	}
+}
---
+++ EjerciciosVariables/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,16 @@
+package ClasesVariables;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		double a=8;
+		int b=1;
+		String c="Colombia";
+		char d='A';
+		float e=74546;
+		String altura="1.85";
+		double alturaReal=Double.parseDouble(altura);
+	}
+
+}
